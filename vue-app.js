cdb9ae301f22db1bd0d8328fd512def5dd753b17
Adam Biro <adam.biro@gmail.com>
2019-09-30 11:55:05 +0100

    We changed the component props Array type to Object. We can define the data type and we can add extra validation to the property.

--- vue-app.js
+++ vue-app.js
@@ -1,13 +1,16 @@
 // Single name components without dash is considered a bad practise however
 // we go with it like plan for simplicity reason
 
-Vue.component('plan',{
-    template:'#plan-template',
-    props:['name']
+Vue.component ('plan', {
+    template: '#plan-template',
+    props: {
+        name: {
+            type: String, required: true
+        }
+    }
 });
 new Vue ({
-    el: '#app',
-    data:{
-        plans:['The Single','The Curious','The Addict']
+    el: '#app', data: {
+        plans: [ 'The Single', 'The Curious', 'The Addict' ]
     }
 });
