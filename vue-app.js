5c70c231a2b6d0ba41b7d1b82ad803519da98cb5
Adam Biro <adam.biro@gmail.com>
2019-09-30 12:33:46 +0100

    Register the plan component as a local component. This way it will not be available outside the plan-picker context. If you were using is globally and you do not need it it still will be converted to the final build.

--- vue-app.js
+++ vue-app.js
@@ -1,24 +1,25 @@
 // We can register a component Globally or Locally.
-// This vue.component approach is the global one and it is not a good practise!!!
-
-Vue.component ('plan-picker', {
-    template: '#plan-picker-template', data() {
-        return {
-            // we could pass this data through props from the Vue root component but
-            // in this way the component independent from the root.
-            plans: [
-                'The Single',
-                'The Curious',
-                'The Addict'
-            ]
-        }
-    }
-});
-
-Vue.component ('plan', {
+// This is the local registration of a component but now
+// can not use the plan component outside of the plan-picker context because we will get an error!
+let PlanComponent;
+PlanComponent = {
     template: '#plan-template', props: {
         name: {
             type: String, required: true
+        }
+    }
+};
+
+Vue.component ('plan-picker', {
+    template: '#plan-picker-template', components: {
+        // keys are the name of the component
+        // the value is the option object
+        plan: PlanComponent
+    }, data() {
+        return {
+            // we could pass this data through props from the Vue root component but
+            // in this way the component independent from the root.
+            plans: [ 'The Single', 'The Curious', 'The Addict' ]
         }
     }
 });
