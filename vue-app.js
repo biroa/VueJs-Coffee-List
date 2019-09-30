821fc602fc039a94dfcc8ac1ba13c91a90e1b94a
Adam Biro <adam.biro@gmail.com>
2019-09-30 12:14:02 +0100

    We wrapped our component with a plan-picker component. We pass data from the wrapper plan-picker component to the plan component. The original plan component is reusable now without compromises.

--- vue-app.js
+++ vue-app.js
@@ -1,16 +1,27 @@
 // Single name components without dash is considered a bad practise however
 // we go with it like plan for simplicity reason
 
+Vue.component ('plan-picker', {
+    template: '#plan-picker-template', data() {
+        return {
+            // we could pass this data through props from the Vue root component but
+            // in this way the component independent from the root.
+            plans: [
+                'The Single',
+                'The Curious',
+                'The Addict'
+            ]
+        }
+    }
+})
+
 Vue.component ('plan', {
-    template: '#plan-template',
-    props: {
+    template: '#plan-template', props: {
         name: {
             type: String, required: true
         }
     }
 });
 new Vue ({
-    el: '#app', data: {
-        plans: [ 'The Single', 'The Curious', 'The Addict' ]
-    }
+    el: '#app'
 });
