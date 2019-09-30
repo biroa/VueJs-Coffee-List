4ddb2e15bad40a8b17377d52aaeb7c4410b6c029
Adam Biro <adam.biro@gmail.com>
2019-09-30 11:23:12 +0100

    We reduced the HTML DOM size with a component. We added parameters to the component from outside with the help of the props param.

--- vue-app.js
+++ vue-app.js
@@ -1,1 +1,10 @@
+// Single name components without dash is considered a bad practise however
+// we go with it like plan for simplicity reason
 
+Vue.component('plan',{
+    template:'#plan-template',
+    props:['name']
+});
+new Vue ({
+    el: '#app'
+});
